e8a6336d89a9bec8b330073364822c802938f22c
Kenrick Halff <kenrickhalff@gmail.com>
2021-12-16 12:05:08 +0100

    Renamed e2e test, wrote tests & made screenshots

---
+++ e2e/index.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test'
+
+const url = 'http://localhost:1337'
+
+test.describe('The website loads up', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(url)
+  })
+
+  test('page loaded', async ({ page }) => {
+    await expect(page).toHaveURL(url)
+  })
+
+  test('the footer shows my name', async ({ page }) => {
+    const footer = page.locator('footer')
+    await expect(footer).toContainText('Kenrick Halff')
+  })
+
+  test('the moon is in the sky', async ({ page }) => {
+    const moon = page.locator('css=img[alt="Image of the moon"]')
+    const imagePath = await moon.getAttribute('src') || ''
+
+    await page.goto(`${imagePath[0] === '/' && url}${imagePath}`)
+    expect(await page.locator('img').screenshot()).toMatchSnapshot('moon.png')
+  })
+
+  test('the silhouette is looking at the sky', async ({ page }) => {
+    const silhouette = page.locator('css=img[alt="Silhouette of Kenrick Halff looking into the night sky"]')
+    const imagePath = await silhouette.getAttribute('src')
+    await page.goto(`${url}${imagePath}`)
+    expect(await page.locator('svg').screenshot()).toMatchSnapshot('silhouette.png')
+  })
+
+  test('the stars are in the sky', async ({ page }) => {
+    const stars = page.locator('[data-testid="stars"]')
+    await expect(stars).toBeVisible()
+    
+  })
+
+  test('the stars are twinkling', async ({ page }) => {
+    const twinkling = page.locator('[data-testid="twinkling"]')
+    await expect(twinkling).toBeVisible()
+  })
+})
