ef36facf62f8dc0a9ea52dc495282b1b5a5f1dc2
Kenrick Halff <kenrickhalff@gmail.com>
2021-12-23 12:01:19 +0100

    Same

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const meta = pageProps.fontMatter
   return <Layout><Component {...pageProps} /></Layout>
 }
 
